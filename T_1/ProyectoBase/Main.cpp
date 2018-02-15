fe622c94dd4af665eddd74a0282e30a71cc5ae4d
Anna Ponce Llerda <annaponcellerda@gmail.com>
2018-02-15 18:07:37 +0100

    NON BLOCKING FET

--- T_1/ProyectoBase/Main.cpp
+++ T_1/ProyectoBase/Main.cpp
@@ -208,7 +208,7 @@ int main()
 */
 
 
-//CHAT BLOCKING WITH THREADING (SIN INTERFAZ GRAFICA, DESPUES DE CERRAR CONEXION AUN INTENTA RECIBIR UNA VEZ)
+/*//CHAT BLOCKING WITH THREADING (SIN INTERFAZ GRAFICA, DESPUES DE CERRAR CONEXION AUN INTENTA RECIBIR UNA VEZ)
 
 #include <SFML\Network.hpp>
 #include <iostream>
@@ -322,12 +322,232 @@ int main()
 	socket.disconnect();
 	system("pause");
 	return 0;
-}
+}*/
 
 //--------------------------------------------------------------------------------------------------------------------------------
 
-/*
-//CHAT SIMPLE CON BLOCKING I SIN THREADS (SIN INTERFAZ GRAFICA, ENVIA UN MENAJE Y EL OTRO ESCUCHA Y ASI SUCCESIVAMENTE)
+//INTERFICIE NON BLOCKING
+#include <SFML\Graphics.hpp>
+#include <string>
+#include <iostream>
+#include <vector>
+
+#define MAX_MENSAJES 30
+
+#include <SFML\Network.hpp>
+#include <thread>
+#include <mutex>
+
+sf::TcpSocket socket;
+sf::Socket::Status status;
+std::mutex myMutex;
+bool chat;
+int puerto = 50000;
+std::vector<std::string> aMensajes;
+sf::String mensaje;
+
+void shared_cout(std::string msg, bool received) {
+	std::lock_guard<std::mutex>guard(myMutex); //impedeix acces alhora
+
+	if (msg != "") {
+		if (received) { aMensajes.push_back("Mensaje recibido: " + msg); }
+		else { aMensajes.push_back(msg); }
+	}
+}
+
+int main()
+{
+	/////////////////////////////////
+	std::cout << "¿Seras servidor (s) o cliente (c)? ... " << std::endl;
+	char who;
+	std::cin >> who;
+	std::string textoAEnviar = "";
+
+	if (who == 's')
+	{
+		sf::TcpListener listener;
+		status = listener.listen(puerto);
+		if (status != sf::Socket::Done)
+		{
+			std::cout << "No se puede vincular con el puerto" << std::endl;
+		}
+
+		if (listener.accept(socket) != sf::Socket::Done) //se queda bloquado el thread hasta que m'envien una connexio --> pq no bloquegi: listener.setblocking(false) --> aixo es non-blocking
+		{
+			std::cout << "Error al aceptar conexion" << std::endl;
+		}
+		else {
+			chat = true;
+		}
+
+		listener.close(); //ya no hace falta porque no hay mas solicitudes de conexion
+	}
+	else if (who == 'c')
+	{
+		status = socket.connect("localhost", puerto, sf::milliseconds(15.f)); //bloqueo durante un tiempo
+		if (status != sf::Socket::Done)
+		{
+			std::cout << "No se ha podido conectar" << std::endl;
+		}
+		else {
+			chat = true;
+		}
+
+	}
+	else
+	{
+		exit(0);
+	}
+
+	//se muestra por pantalla con quien se ha hecho la conexion, tanto en el server como en el cliente
+	std::string texto = "Conexion con ... " + (socket.getRemoteAddress()).toString() + ":" + std::to_string(socket.getRemotePort()) + "\n";
+	std::cout << texto;
+
+	////////////////////////////////
+	socket.setBlocking(false);
+
+	sf::Vector2i screenDimensions(800, 600);
+
+	sf::RenderWindow window;
+	window.create(sf::VideoMode(screenDimensions.x, screenDimensions.y), "Chat");
+
+	sf::Font font;
+	if (!font.loadFromFile("calibri.ttf"))
+	{
+		std::cout << "Can't load the font file" << std::endl;
+	}
+
+	mensaje = "";
+
+	sf::Text chattingText(mensaje, font, 14);
+	chattingText.setFillColor(sf::Color(0, 160, 0));
+	chattingText.setStyle(sf::Text::Bold);
+
+
+	sf::Text text(mensaje, font, 14);
+	text.setFillColor(sf::Color(0, 160, 0));
+	text.setStyle(sf::Text::Bold);
+	text.setPosition(0, 560);
+
+	sf::RectangleShape separator(sf::Vector2f(800, 5));
+	separator.setFillColor(sf::Color(200, 200, 200, 255));
+	separator.setPosition(0, 550);
+
+	while (window.isOpen())
+	{
+		sf::Event evento;
+		if (chat) {
+			char buffer[100];
+			size_t bytesReceived;
+
+			status = socket.receive(buffer, 100, bytesReceived); //bloquea el thread principal hasta que no llegan los datos
+			//if(status == sf::Socket::isReady)
+			
+			if (status == sf::Socket::Done)
+			{
+				buffer[bytesReceived] = '\0';
+				shared_cout(buffer, true); //se muestra por pantalla lo recibido
+
+			}
+			else if (status == sf::Socket::Disconnected)
+			{
+				
+				shared_cout("Desconectado", false); //se muestra por pantalla lo recibido
+				chat = false;
+			}
+		}
+		while (window.pollEvent(evento))
+		{
+
+			switch (evento.type)
+			{
+			case sf::Event::Closed:
+				window.close();
+				break;
+			case sf::Event::KeyPressed:
+				if (evento.key.code == sf::Keyboard::Escape)
+					window.close();
+				else if (evento.key.code == sf::Keyboard::Return)
+				{
+					////////////////////
+					if (chat) {
+						
+						////////////////////------------------------------------------------
+						std::string s_mensaje;
+						size_t bSent;
+
+						if (mensaje == "exit") {
+							s_mensaje = "Chat finalizado";
+						}
+						else {
+							s_mensaje = mensaje;
+						}
+
+						status = socket.send(s_mensaje.c_str(), s_mensaje.length(), bSent);
+						shared_cout(mensaje, false);
+
+						if (status != sf::Socket::Done)
+						{
+							if (status == sf::Socket::Error)
+								shared_cout("Ha fallado el envio", false);
+							else if (status == sf::Socket::Disconnected)
+								shared_cout("Disconnected", false);
+							else if (status == sf::Socket::Partial) {
+								//copiar aqui merdeeeee
+								while (bSent < textoAEnviar.size()) {
+									std::string msgRest = "";
+									for (size_t i = bSent; i < textoAEnviar.size(); i++) {
+										msgRest = textoAEnviar[i];
+									}
+									socket.send(msgRest.c_str(), msgRest.size(), bSent);
+								}
+							}
+						}
+						
+						if (mensaje == "exit") {
+							chat = false;
+							socket.disconnect();
+						}
+					}
+					///////////////////
+					if (aMensajes.size() > 25)
+					{
+						aMensajes.erase(aMensajes.begin(), aMensajes.begin() + 1);
+					}
+					mensaje = "";
+				}
+				break;
+			case sf::Event::TextEntered:
+				if (evento.text.unicode >= 32 && evento.text.unicode <= 126)
+					mensaje += (char)evento.text.unicode;
+				else if (evento.text.unicode == 8 && mensaje.getSize() > 0)
+					mensaje.erase(mensaje.getSize() - 1, mensaje.getSize());
+				break;
+			}
+		}
+
+		window.draw(separator);
+		for (size_t i = 0; i < aMensajes.size(); i++)
+		{
+			std::string chatting = aMensajes[i];
+			chattingText.setPosition(sf::Vector2f(0, 20 * i));
+			chattingText.setString(chatting);
+			window.draw(chattingText);
+		}
+
+		std::string mensaje_ = mensaje + "_";
+		text.setString(mensaje_);
+		window.draw(text);
+
+
+		window.display();
+		window.clear();
+	}
+}
+
+
+
+/*//CHAT SIMPLE CON NON BLOCKING Y SIN THREADS (SIN INTERFAZ GRAFICA, ENVIA UN MENAJE Y EL OTRO ESCUCHA Y ASI SUCCESIVAMENTE)
 
 #include <SFML\Network.hpp>
 #include <iostream>
@@ -404,16 +624,10 @@ int main()
 
 		status = socket.receive(buffer, 100, bytesReceived); //bloquea el thread principal hasta que no llegan los datos
 
-		if (status == sf::Socket::NotReady) { //es queda aqui atrapat holy shit
-			//std::cout << "Not Ready. " << std::endl;
-			continue;
-		}
-		else if (status == sf::Socket::Error) {
+		if (status == sf::Socket::Error) {
 			shared_cout("Error. ", false);
 		}
-		else if (status == sf::Socket::Partial) {
-			shared_cout("Partial. ", false);
-		}
+
 		else if (status == sf::Socket::Done)
 		{
 			buffer[bytesReceived] = '\0';
@@ -426,10 +640,21 @@ int main()
 
 
 		std::getline(std::cin, textoAEnviar);
-		status = socket.send(textoAEnviar.c_str(), texto.length());
+		size_t bSent;
+		status = socket.send(textoAEnviar.c_str(), textoAEnviar.size(), bSent);
 
 		if (status != sf::Socket::Done)
 		{
+			if (status == sf::Socket::Partial) {
+
+				while (bSent < textoAEnviar.size()) {
+					std::string msgRest = "";
+					for (int i = bSent; i >= textoAEnviar.length(); i--) {
+						msgRest = textoAEnviar[i];	
+					}	
+					socket.send(msgRest.c_str(), msgRest.size(), bSent);
+				}
+			}
 			shared_cout("Ha fallado el envio", false);
 		}
 
@@ -439,8 +664,8 @@ int main()
 	system("pause");
 
 	return 0;
-}
-*/
+}*/
+
 
 //--------------------------------------------------------------------------------------------------------------------------------
 /*
