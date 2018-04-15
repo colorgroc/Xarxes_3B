1d4ad653dcf99b1673edcffd1d9622e906566e75
colorgroc <annaponcellerda@gmail.com>
2018-04-15 15:04:20 +0200

    Solucionat, crec, error en eliminar elements de la llista

--- UDP/UDP_Game_Libreria/GameServer/Server.cpp
+++ UDP/UDP_Game_Libreria/GameServer/Server.cpp
@@ -256,6 +256,8 @@ void PositionValidations() {
 	//enviar amb OkPosition
 	//reenviar a tots els altres jugadors
 	//REFRESH_POSITIONS 
+	int8_t posToDelete;
+
 	for (std::map<int8_t, Client>::iterator client = clients.begin(); client != clients.end(); ++client) {
 
 		for (std::map<int8_t, std::pair<int8_t, AccumMovements>>::iterator pos = client->second.MapAccumMovements.begin(); pos != client->second.MapAccumMovements.end(); ++pos) {
@@ -274,10 +276,16 @@ void PositionValidations() {
 				packet.clear();
 
 				//borrem el accum analitzat de la llista del client
-				client->second.MapAccumMovements.erase(pos->first);
+				posToDelete = pos->first;
+				//client->second.MapAccumMovements.erase(pos->first);
 			}
 		}
 
+	}
+	for (int8_t i = 1; i <= clients.size(); i++) {
+		if (clients.find(i) != clients.end() && clients[i].MapAccumMovements.find(posToDelete) != clients[i].MapAccumMovements.end()) {
+			clients[i].MapAccumMovements.erase(posToDelete);
+		}
 	}
 }
 
